2c07bde88d64c6ce2a7fbe4a012aa067883f59b2
Tun Aung Myint <kakyi99999@gmail.com>
2022-09-01 18:41:49 +0630

    loading bar

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -4,14 +4,20 @@ import { StarIcon } from '@heroicons/react/solid';
 
 function InfoCard({ img, location, title, description, star, price, total }) {
   return (
-    <div className="flex py-7 px-2 pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t  ">
+    <div className="flex py-7 px-2 pr-4 border-b hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t  ">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
-        <Image src={img} layout="fill" objectFit="cover" alt={title} />
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="cover"
+          alt={title}
+          className="rounded-lg"
+        />
       </div>
       <div className="flex flex-col flex-grow pl-5">
         <div className="flex justify-between">
           <p>{location}</p>
-          <HeartIcon className="h-7 cursor-pointer" />
+          <HeartIcon className="h-7 cursor-pointer text-red-400" />
         </div>
 
         <h4 className="text-lg">{title}</h4>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import ProgressBar from '@badrap/bar-of-progress';
+import { Router } from 'next/router';
+
+const progress = new ProgressBar({
+  size: 2,
+  color: '#FD5B61',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.start);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
